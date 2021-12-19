97ad135ae52e2e60f74578510a8b327fdc74c1a6
buggsboony <boonydeboon@gmail.com>
2021-12-19 12:57:50 -0400

    Desktop launcher OK

--- install.sh
+++ install.sh
@@ -3,10 +3,23 @@
 #install stuff
 what=${PWD##*/}   
 extension=.sh
+
+#config
+configpath=~/.config/goscrcpy/
 #peut être extension vide
 
 echo "Set executable..."
 chmod +x $what$extension
 #echo "lien symbolique vers usr bin"
 sudo ln -s "$PWD/$what$extension" /usr/bin/$what
+
+#copy icon
+echo "Create directory /usr/share/goscrcpy..."
+sudo mkdir -p /usr/share/goscrcpy
+echo "Copy icon .."
+sudo cp smartphone-pngfind.com-blue-phone-icon-png-5193500.png /usr/share/goscrcpy/icon.png
+echo "Your desktop file: "
+mkdir -p "$configpath"
+cp goscrcpy_gui.desktop_original  $configpath/goscrcpy_gui.desktop
+xdg-open "$configpath/goscrcpy_gui.desktop"
 echo "done."
--- uninstall.sh
+++ uninstall.sh
@@ -10,6 +10,7 @@ killall $what
 
 echo "remove symbolic link from usr bin"
 sudo rm /usr/bin/$what
-
+echo "Clear directory ... /usr/share/goscrcpy/*"
+sudo rm -rf /usr/share/goscrcpy/* #Clear directory
 echo "done."
 
